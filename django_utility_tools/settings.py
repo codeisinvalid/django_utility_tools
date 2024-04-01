de17028ba5cb071fdc4e82b387b809066e557e36
sooj <soojksh@gmail.com>
2024-04-01 23:47:44 +0530

    apps created for various utilities

--- django_utility_tools/settings.py
+++ django_utility_tools/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'urlshortener',
+    'imageresizer',
+    'domainnamechecker',
+    'taskmanager',
 ]
 
 MIDDLEWARE = [
