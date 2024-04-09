8711a51451e4f7ae8fbe1acd3b4aa1fe4656da79
sooj <soojksh@gmail.com>
2024-04-09 15:37:34 +0530

    sending image to the django backend

--- imageresizer/views.py
+++ imageresizer/views.py
@@ -6,8 +6,19 @@ import io
 import json
 # Create your views here.
 def image_resizer(request):
+    if request.method=="GET":
 
-    return render(request, 'imageresizer/imageresizer.html')
+        return render(request, 'imageresizer/imageresizer.html')
+    elif request.method=='POST':
+        try:
+            image_data = request.body
+            return JsonResponse({'success': True, 'message': 'Image data received successfully.'})
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': str(e)}, status=400)
+    else:
+        # Return a method not allowed response
+        return JsonResponse({'success': False, 'message': 'Method not allowed.'}, status=405)
+
 
 @csrf_exempt
 def image_editor(request):
